a2e2fde265e1c9d83b780805277069448ef2907d
hong2004 <150147860+hong3497@users.noreply.github.com>
2025-05-07 12:39:46 +0900

    restore: app folder recovered

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
